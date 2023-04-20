d036049f0cd10152094ae53c57c6d1e153e2a836
FLORENTIN TOUPET <FLORENTINTOUPET@GMAIL.COM>
2023-04-19 22:21:17 -0400

    Push Final

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const courseController = require('../controllers/courseController');
 
 router.post('/', courseController.create); // Yup
-router.get('/:id', courseController.getById); //
-router.patch('/:id', courseController.update); //
-router.delete('/:id', courseController.delete); //
+router.get('/:id', courseController.getById); // Yup
+router.patch('/:id', courseController.update); // Yup
+router.delete('/:id', courseController.delete); // Yup
 
 module.exports = router;
--- routes/professorRoutes.js
+++ routes/professorRoutes.js
@@ -2,11 +2,10 @@ const express = require('express');
 const router = express.Router();
 const professorController = require('../controllers/professorController');
 
-router.post('/', professorController.create);
-router.get('/:id', professorController.getById);
-router.put('/:id', professorController.update);
-router.delete('/:id', professorController.deleteProf);
-router.patch('/:id/ajouterCours',professorController.ajouterCours)
-// router.patch("/cours/:professeurId", professorController.ajouterCours);
+router.post('/', professorController.create); // Yup
+router.get('/:id', professorController.getById); // Yup
+router.put('/:id', professorController.update); // Yup
+router.delete('/:id', professorController.deleteProf); // Yup
+router.patch('/:id/ajouterCours',professorController.ajouterCours) //Yup
 
 module.exports = router;
